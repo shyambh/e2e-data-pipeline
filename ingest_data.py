f385bd11fc4b424b00e5d3122150661c56cf5681
shyam <bhattaraishyam@gmail.com>
2023-12-23 22:47:30 +0545

    Add code to upload the dataframe to GCP BigQuery

--- ingest_data.py
+++ ingest_data.py
@@ -8,6 +8,7 @@ import pandas as pd
 from prefect import flow, task
 from prefect_sqlalchemy import SqlAlchemyConnector
 from prefect_gcp.cloud_storage import GcsBucket, DataFrameSerializationFormat
+from prefect_gcp import GcpCredentials
 
 
 def get_filename(filename):
@@ -18,11 +19,12 @@ def get_filename(filename):
 def download_csv(url_to_csv, output_file_name):
     """Function to download the csv file from remote"""
     os.system(f"curl -L -o {output_file_name} {url_to_csv}")
-    os.system(f"gzip --decompress {output_file_name}")
+    os.system(f"gzip --decompress --force {output_file_name}")
 
 
-@task(log_prints=True)
+@task(name="", log_prints=True)
 def transform_data(df):
+    """Transform the data before dumping in the DB"""
     print(f"pre:missing passenger count: {df['passenger_count'].isin([0]).sum()}")
 
     df = df[df["passenger_count"] != 0]
@@ -74,25 +76,45 @@ def dump_to_db(table_name, filename):
 
 
 @flow(
-    name="GCS Data Loader",
-    description="Upload the csv file in the parquet format in GCP cloud bucket",
+    name="Store to Cloud",
+    description="Upload the csv file in the parquet format in GCP cloud bucket & BigQuery",
 )
-def store_to_gcp_bucket() -> None:
-    """Load the first 100,000 records into GCS Bucket"""
+def store_to_cloud() -> None:
+    """Load the first 100,000 records into GCS Bucket & BigQuery"""
+    sql_connection_block = SqlAlchemyConnector.load("pg-sql-connector")
     gcp_bucket_block = GcsBucket.load("gcp-zoomcamp-bucket")
+    gcp_credentials_block = GcpCredentials.load("gcp-de-zoomcamp-creds")
 
-    connection_block = SqlAlchemyConnector.load("pg-sql-connector")
-
-    with connection_block.get_connection() as sql_con:
+    with sql_connection_block.get_connection() as sql_con:
         df = pd.read_sql_table("yellow_taxi_table", con=sql_con, chunksize=100000)
 
         df_trimmed = next(df)
         df_trimmed.to_parquet("./.sample_data/output.parquet", compression="gzip")
-        gcp_bucket_block.upload_from_dataframe(
-            df_trimmed,
-            to_path="data/output.parquet",
-            serialization_format=DataFrameSerializationFormat.PARQUET_GZIP,
-        )
+
+    df_trimmed.to_gbq(
+        "ny_taxi_data_2021.rides",
+        "data-eng-practice007",
+        chunksize=100_000,
+        credentials=gcp_credentials_block.get_credentials_from_service_account(),
+        if_exists="append",
+    )
+    gcp_bucket_block.upload_from_dataframe(
+        df_trimmed,
+        to_path="data/output.parquet",
+        serialization_format=DataFrameSerializationFormat.PARQUET_GZIP,
+    )
+
+    download_from_gcs_to_local()
+
+
+@task(name="Download file from GCS to local")
+def download_from_gcs_to_local():
+    """Download the uploaded parquet file from GCS"""
+    gcp_bucket_block = GcsBucket.load("gcp-zoomcamp-bucket")
+    gcp_bucket_block.get_directory(
+        "data/output.gz.parquet",
+        ".sample_data/downloaded/data/output.gz.parquet",
+    )
 
 
 @flow(name="Ingest Flow")
@@ -109,7 +131,7 @@ def main_flow():
 
     dump_to_db(args.tbl_name, filename)
 
-    store_to_gcp_bucket()
+    store_to_cloud()
 
 
 if __name__ == "__main__":
